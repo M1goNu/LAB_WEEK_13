c2f070a52e3d64b282843b5edaa8b047c1538a33
M1goNu <rifqi.aldino71@gmail.com>
2025-12-03 10:44:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_13/MovieViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.lab_week_13
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import com.example.lab_week_13.model.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import java.util.Calendar
+
+class MovieViewModel(private val movieRepository: MovieRepository)
+    : ViewModel() {
+    init {
+        fetchPopularMovies()
+    }
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
+    private val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
+
+    val filteredMovies: StateFlow<List<Movie>> =
+        popularMovies
+            .map { movies ->
+                movies
+                    .filter { movie ->
+                        movie.releaseDate?.startsWith(currentYear) == true
+                    }
+                    .sortedByDescending { it.popularity }
+            }
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(),
+                emptyList()
+            )
+
+    // define the StateFlow in replace of the LiveData
+    // a StateFlow is an observable Flow that emits state updates to the collectors
+    // MutableStateFlow is a StateFlow that you can change the value
+    // fetch movies from the API
+    private fun fetchPopularMovies() {
+        // launch a coroutine in viewModelScope
+        // Dispatchers.IO means that this coroutine will run on a shared pool of threads
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies().catch {
+                // catch is a terminal operator that catches exceptions from the Flow
+                _error.value = "An exception occurred: ${it.message}"
+            }.collect {
+                // collect is a terminal operator that collects the values from the Flow
+                // the results are emitted to the StateFlow
+                _popularMovies.value = it
+            }
+        }
+    }
+}
